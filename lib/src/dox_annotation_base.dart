0a95cb4caa11ad02a512c73dcd0cf58b0cf82e2b
AJ <aj.zinkyaw@gmail.com>
2023-05-14 17:40:15 +0700

    bump 0.0.3

--- lib/src/dox_annotation_base.dart
+++ lib/src/dox_annotation_base.dart
@@ -4,15 +4,3 @@ class DoxModel {
 
   const DoxModel({this.table, this.primaryKey});
 }
-
-class Column {
-  final String? name;
-  final Function? beforeSave;
-  final Function? beforeGet;
-
-  const Column({
-    this.name,
-    this.beforeSave,
-    this.beforeGet,
-  });
-}
