211e2f104a8cc5142b330118a76e9367577a531e
AJ <aj.zinkyaw@gmail.com>
2023-05-15 21:31:43 +0700

    added config for custom created at ad updated column

--- lib/src/dox_annotation_base.dart
+++ lib/src/dox_annotation_base.dart
@@ -1,6 +1,8 @@
 class DoxModel {
   final String? table;
   final String? primaryKey;
+  final String? createdAt;
+  final String? updatedAt;
 
-  const DoxModel({this.table, this.primaryKey});
+  const DoxModel({this.table, this.primaryKey, this.createdAt, this.updatedAt});
 }
